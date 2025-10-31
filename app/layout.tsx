148e3c7a7e895bad03b550b0f0fd75f830995f15
DouglasEmmanuel <emmanueldouglas2121@gmail.com>
2025-10-31 13:29:47 -0700

    added images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Manrope, Roboto } from 'next/font/google';
 
+const manrope = Manrope({ subsets: ['latin'], variable: '--font-manrope' });
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' });
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${manrope.variable} ${roboto.variable} antialiased`}
       >
         {children}
       </body>
